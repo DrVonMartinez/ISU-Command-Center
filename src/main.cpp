7ed5601ff39ed9e8c85546e4db878013bf625153
Dthomas810 <32943574+Dthomas810@users.noreply.github.com>
2017-12-09 21:09:11 -0600

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
-#include "sc2api/sc2_api.h"
-#include "sc2utils/sc2_manage_process.h"
-#include "rapidjson/document.h"
+#include "Common.h"
+#include "CCBot.h"
 #include "JSONTools.h"
 #include "Util.h"
 
-#include <iostream>
-#include <string>
-#include <random>
-#include <cmath>
+#ifdef SC2API
 
-#include "CCBot.h"
-
+#include "sc2utils/sc2_manage_process.h"
+#include "sc2api/sc2_api.h"
 
 int main(int argc, char* argv[]) 
 {
+	
     sc2::Coordinator coordinator;
     if (!coordinator.LoadSettings(argc, argv)) 
     {
@@ -21,7 +18,6 @@ int main(int argc, char* argv[])
         return 1;
     }
     
-    rapidjson::Document doc;
     std::string config = JSONTools::ReadFile("BotConfig.txt");
     if (config.length() == 0)
     {
@@ -30,26 +26,31 @@ int main(int argc, char* argv[])
         exit(-1);
     }
 
-    bool parsingFailed = doc.Parse(config.c_str()).HasParseError();
-    if (parsingFailed)
+    std::ifstream file("BotConfig.txt");
+    json j;
+    file >> j;
+
+    /*if (parsingFailed)
     {
         std::cerr << "Config file could not be parsed, and is required for starting the bot\n";
         std::cerr << "Please read the instructions and try again\n";
         exit(-1);
-    }
+    }*/
 
     std::string botRaceString;
     std::string enemyRaceString;
     std::string mapString;
     int stepSize = 1;
+    sc2::Difficulty enemyDifficulty = sc2::Difficulty::Easy;
 
-    if (doc.HasMember("Game Info") && doc["Game Info"].IsObject())
+    if (j.count("SC2API") && j["SC2API"].is_object())
     {
-        const rapidjson::Value & info = doc["Game Info"];
+        const json & info = j["SC2API"];
         JSONTools::ReadString("BotRace", info, botRaceString);
         JSONTools::ReadString("EnemyRace", info, enemyRaceString);
         JSONTools::ReadString("MapFile", info, mapString);
         JSONTools::ReadInt("StepSize", info, stepSize);
+        JSONTools::ReadInt("EnemyDifficulty", info, enemyDifficulty);
     }
     else
     {
@@ -66,11 +67,11 @@ int main(int argc, char* argv[])
     //          Setting this = N means the bot's onFrame gets called once every N frames
     //          The bot may crash or do unexpected things if its logic is not called every frame
     coordinator.SetStepSize(stepSize);
-    coordinator.SetRealtime(false);
+    coordinator.SetRealtime(true);
 
     coordinator.SetParticipants({
-        CreateParticipant(Util::GetRaceFromString(botRaceString), &bot),
-        CreateComputer(Util::GetRaceFromString(enemyRaceString))
+        sc2::CreateParticipant(Util::GetRaceFromString(botRaceString), &bot),
+        sc2::CreateComputer(Util::GetRaceFromString(enemyRaceString), enemyDifficulty)
     });
 
     // Start the game.
@@ -85,3 +86,102 @@ int main(int argc, char* argv[])
 
     return 0;
 }
+
+#else
+
+#include <BWAPI/Client.h>
+
+void UAlbertaBot_BWAPIReconnect() 
+{
+    while(!BWAPI::BWAPIClient.connect())
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds{ 1000 }); 
+    }
+}
+
+void UAlbertaBot_PlayGame()
+{
+    CCBot bot;
+    
+    // The main game loop, which continues while we are connected to BWAPI and in a game
+	while (BWAPI::BWAPIClient.isConnected() && BWAPI::Broodwar->isInGame()) 
+    {
+        // Handle each of the events that happened on this frame of the game
+		for (const BWAPI::Event & e : BWAPI::Broodwar->getEvents()) 
+        {
+			switch (e.getType()) 
+            {
+                case BWAPI::EventType::MatchStart:      { bot.OnGameStart();                 break; }
+			    case BWAPI::EventType::MatchFrame:      { bot.OnStep();                      break; }
+                                                        
+                /*case BWAPI::EventType::MatchEnd:        { m->onEnd(e.isWinner());            break; }
+			    case BWAPI::EventType::UnitShow:        { m->onUnitShow(e.getUnit());        break; }
+			    case BWAPI::EventType::UnitHide:        { m->onUnitHide(e.getUnit());        break; }
+			    case BWAPI::EventType::UnitCreate:      { m->onUnitCreate(e.getUnit());      break; }
+			    case BWAPI::EventType::UnitMorph:       { m->onUnitMorph(e.getUnit());       break; }
+			    case BWAPI::EventType::UnitDestroy:     { m->onUnitDestroy(e.getUnit());     break; }
+			    case BWAPI::EventType::UnitRenegade:    { m->onUnitRenegade(e.getUnit());    break; }
+			    case BWAPI::EventType::UnitComplete:    { m->onUnitComplete(e.getUnit());    break; }
+			    case BWAPI::EventType::SendText:        { m->onSendText(e.getText());        break; }*/
+			}
+		}
+
+		BWAPI::BWAPIClient.update();
+		if (!BWAPI::BWAPIClient.isConnected()) 
+        {
+			std::cout << "Disconnected\n";
+			break;
+		}
+	}
+
+    std::cout << "Game Over\n";
+}
+
+int main(int argc, char * argv[]) 
+{
+    bool exitIfStarcraftShutdown = true;
+
+    size_t gameCount = 0;
+	while (true)
+    {
+        // if we are not currently connected to BWAPI, try to reconnect
+        if (!BWAPI::BWAPIClient.isConnected())
+        {
+            UAlbertaBot_BWAPIReconnect();
+        }
+
+        // if we have connected to BWAPI
+        while (BWAPI::BWAPIClient.isConnected())
+        {
+            // wait for the game to start until the game starts
+            std::cout << "Waiting for game start\n";
+	        while (BWAPI::BWAPIClient.isConnected() && !BWAPI::Broodwar->isInGame()) 
+            {
+		        BWAPI::BWAPIClient.update();
+	        }
+
+            // Check to see if Starcraft shut down somehow
+            if (BWAPI::BroodwarPtr == nullptr)
+            {
+                break;
+            }
+
+            // If we are successfully in a game, call the module to play the game
+            if (BWAPI::Broodwar->isInGame())
+            {
+                std::cout << "Playing game " << gameCount++ << " on map " << BWAPI::Broodwar->mapFileName() << "\n";
+
+                UAlbertaBot_PlayGame();
+            }
+        }
+
+        if (exitIfStarcraftShutdown && !BWAPI::BWAPIClient.isConnected())
+        {
+            return 0;
+        }
+    }
+
+	return 0;
+}
+
+#endif
